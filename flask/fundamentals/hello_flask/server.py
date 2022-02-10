37d18d3f172d79ce429b2407439a4dffab9fbfac
donovanlem <kiloatw@gmail.com>
2022-02-10 14:34:29 -0700

    flask practice

--- flask/fundamentals/hello_flask/server.py
+++ flask/fundamentals/hello_flask/server.py
@@ -1,8 +1,26 @@
+from doctest import OutputChecker
 from flask import Flask  
 app = Flask(__name__)    
 @app.route('/')          
 def hello_world():
-    return 'Hello World!'  
+    return 'Hello World!' 
+
+@app.route('/dojo')
+def dojo():
+    return 'Dojo!'
+
+@app.route('/say/<string:name>')
+def say_hi(name):
+    return f'Hi {name.capitalize()}!'
+
+@app.route('/repeat/<int:num>/<string:word>')
+def repeat(num, word):
+    words = ''
+    for i in range(0,num):
+        words += f"<p>{word}</p>"
+    return words
+
+
 if __name__=="__main__":   
     app.run(debug=True)    
 
