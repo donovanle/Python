6f097f5c73602bad34e9cce39f42216912e95ca0
donovanlem <kiloatw@gmail.com>
2022-02-17 20:16:02 -0700

    dojos_ninjas

--- zdojos_ninjas/dojo.py
+++ zdojos_ninjas/dojo.py
@@ -40,5 +40,5 @@ class Ninja():
     @classmethod
     def createninja(cls, data):
         query = "INSERT INTO ninjas (first_name, last_name, age, dojo_id, created_at, updated_at) " \
-            "VALUES (%(name)s, NOW(), NOW())"
+            "VALUES (%(first_name)s,%(last_name)s,%(age)s,%(dojo_id)s, NOW(), NOW())"
         return connectToMySQL('dojos_and_ninjas_schema').query_db(query, data)
--- zdojos_ninjas/server.py
+++ zdojos_ninjas/server.py
@@ -2,8 +2,6 @@ from flask import render_template,redirect,request,session,Flask
 from dojo import Dojo, Ninja
 app = Flask(__name__)
 
-
-
 @app.route("/dojos")
 def index():
     dojos = Dojo.get_all_dojos()
@@ -19,6 +17,16 @@ def savedojo():
 def showdojo(dojo_id):
     data = { "id": dojo_id}
     return render_template("allninjas.html" , ninjas=Ninja.get_one_dojo(data))
+
+@app.route('/ninja')
+def showninjna_form():
+    dojos = Dojo.get_all_dojos()
+    return render_template("newninja.html" , all_dojos=dojos)
+
+@app.route("/saveninja", methods=["POST"])
+def saveninja():
+    Ninja.createninja(request.form)
+    return redirect("/dojos")
 
 if __name__ == "__main__":
     app.run(debug=True)
